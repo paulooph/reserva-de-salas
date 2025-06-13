65fb950a3835226c1a74c7c6483fc5f9333cf370
Paulo Leme <paulo.oph@hotmail.com>
2025-06-13 15:21:42 -0300

    Initial commit: projeto ASP .NET Core MVC

---
+++ reserva-de-salas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace reserva_de_salas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
